b5f4c40c8e76f46a58782d5621b1b076c5ed455b
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-01-29 20:25:43 +0530

    findOneAndUpdate by email

--- src/app.js
+++ src/app.js
@@ -39,20 +39,36 @@ app.delete("/user", async (req, res) => {
 })
 
 // Update a user in the database
+// app.patch("/user", async (req, res) => {
+//   const userId = req.body.userId;
+//   const data = req.body;
+//   try {
+//    const user =  await User.findByIdAndUpdate({ _id: userId }, data, {
+//       returnDocument: "before"
+//     });
+//     console.log(user)
+//     res.send("User updated successfully");
+//   } catch (err) {
+//     res.status(400).send("error in creating user:");
+//   }
+// })
+
+// Update a user by emailId
 app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
   const data = req.body;
+  const emailId = req.body.emailId;
   try {
-   const user =  await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "before"
-    });
-    console.log(user)
+    const user = await User.findOneAndUpdate(
+      { "emailId": emailId },
+      data,
+      { new: true }
+    );
+    console.log(user);
     res.send("User updated successfully");
   } catch (err) {
-    res.status(400).send("error in creating user:");
+    res.status(400).send("error in updating user:");
   }
-})
-
+});
 // Get user by email
 app.get("/user", async (req, res) => {
   try {
