03831b3c19ddac5b0a92a4f2f7daf3c39485346d
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-04-08 23:26:53 +0530

    Database.js merge  error solved

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
+
 const connectDB = async () => {
-<<<<<<< HEAD
-  await mongoose.connect(
-    "mongodb+srv://BavnitSingh:aARilV4x6rClQS8N@namastenode.n4a4r.mongodb.net/devTinder"
-  );
-=======
   await mongoose.connect(process.env.MONGODB_URI);
->>>>>>> e3e3e0c (Production Build)
-}
+};
+
 module.exports = connectDB;
